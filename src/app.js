c73b4893dbf2549b9890c7b349260a65309dc62d
Viet Hoang <cooro1994@gmail.com>
2019-08-24 15:43:34 +0900

    test out import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router = require('./route')
+import express from 'express';
+import route from './route';
 
 const app = express()
 app.get('/', (req, res) => res.send("Hello World Too"))
